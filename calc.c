7b88f9462f2cb11ff3fd3f6129784f6ed32c0d4c
hanjunm96 <hello@naver.com>
2019-10-08 20:11:35 +0900

    hello

--- calc.c
+++ calc.c
@@ -1,7 +1,14 @@
+
 #include<stdio.h>
+
+int  mul(int num1,int num2){
+	return 2*num1*num2;
+}
+
 
 int main() {
 	int firstNumber, secondNumber;
 	scanf("%d %d", &firstNumber, &secondNumber);
+	
 	return 0;
 }
