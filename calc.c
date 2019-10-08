702e4efba258062f1aa709659806662fc6c67ac1
andytwo98 <andytwo98@naver.com>
2019-10-08 19:58:27 +0900

    곱셈함수 추가

--- calc.c
+++ calc.c
@@ -1,7 +1,10 @@
 #include<stdio.h>
-
+int mult(int num1,int num2){
+	return num1*num2;
+}
 int main() {
 	int firstNumber, secondNumber;
 	scanf("%d %d", &firstNumber, &secondNumber);
+	printf("%d",mult(firstNumber,secondNumber);
 	return 0;
 }
